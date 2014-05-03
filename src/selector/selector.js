c985b47d519bf11b564324ae719193afd4367fbf
bartjoyce <bartholomew.michael.joyce@gmail.com>
2014-05-03 20:10:46 +0100

    Updated selector.js, tree.js

--- src/selector/selector.js
+++ src/selector/selector.js
@@ -1,12 +1,21 @@
 /**
  * selector.js implements:
  * - ROM.selector (object)
+ * - ROM.selector.Selector (constructor)
  */
 
 window.ROM.selector = (function() {
   var selector = {};
 
+  selector.Selector = function Selector(selectorString, tagName, id, classes, attributes, directAncestor) {
+    this.selectorString = selectorString;
 
+    this.match = function match(parentElement) {
+
+    };
+
+    return this;
+  }
 
   return selector;
 })();
--- src/selector/tree.js
+++ src/selector/tree.js
@@ -1,16 +1,13 @@
 /**
  * selector/tree.js implements:
  * - ROM.selector.SelectorTree (constructor)
- * - ROM.selector.SelectorNode (constructor)
- * - ROM.selector.tree (instance of SelectorTree)
+ * - ROM.selector.tree (instance of SelectorNode)
  */
 
 window.ROM.selector.SelectorTree = function SelectorTree() {
+  this.addSelector = function addSelector(selector) {
 
-};
-
-window.ROM.selector.SelectorNode = function SelectorNode() {
-
+  };
 };
 
 window.ROM.selector.tree = new ROM.selector.SelectorTree();
