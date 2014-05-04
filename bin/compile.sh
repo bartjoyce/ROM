2bbed098f69d5d30c6029ecd9c954b6e2aa05e62
bartjoyce <bartholomew.michael.joyce@gmail.com>
2014-05-04 14:16:16 +0100

    Updated compile.sh

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 cd ${0%/*}
 cd ../src
-java -jar ../closurecompiler/compiler.jar --js=main.js --js=selector/selector.js --js=selector/tree.js --js=selector/attributes.js --js=selector/parser.js --js_output_file=../bin/ROM.js
+
+java -jar "../closurecompiler/compiler.jar" \
+--js "main.js" \
+--js "selector/selector.js" \
+--js "selector/tree.js" \
+--js "selector/attributes.js" \
+--js "selector/parser.js" \
+--js_output_file "../bin/ROM.js"
