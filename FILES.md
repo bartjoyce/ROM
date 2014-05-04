a55c3a66b27233a66afc89d2833a856ca181335f
bartjoyce <bartholomew.michael.joyce@gmail.com>
2014-05-04 21:35:42 +0100

    Added files: core/update.js, util/components.js

--- FILES.md
+++ FILES.md
@@ -3,10 +3,12 @@ The following files should be compiled (in order):
 main.js
 core/digest.js
 core/apply.js
+core/update.js
 selector/selector.js
 selector/tree.js
 selector/attributes.js
 selector/parser.js
+util/components.js
 ```
 
 This is basically what ``bin/compile.sh`` does.
--- bin/compile.sh
+++ bin/compile.sh
@@ -6,8 +6,10 @@ java -jar "../closurecompiler/compiler.jar" \
 --js "main.js" \
 --js "core/digest.js" \
 --js "core/apply.js" \
+--js "core/update.js" \
 --js "selector/selector.js" \
 --js "selector/tree.js" \
 --js "selector/attributes.js" \
 --js "selector/parser.js" \
+--js "util/components.js" \
 --js_output_file "../bin/ROM.js"
