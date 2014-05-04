0ec58ad5e96d507539ade9586b0951d3dd0e4fe2
bartjoyce <bartholomew.michael.joyce@gmail.com>
2014-05-04 21:31:17 +0100

    Created components.js

---
+++ src/util/components.js
@@ -0,0 +1,15 @@
+/**
+ * components.js implements:
+ * - ROM.util.getElementComponents (function)
+ */
+window.ROM.util.getElementComponents = (function() {
+  /**
+   * getElementComponents()
+   * Given an element returns the elements that are attached to it.
+   */
+  var getElementComponents = function getElementComponents(element) {
+
+  };
+
+  return getElementComponents;
+})();
